e3e210a521d403ac6155bf5d6a9626cc597e83c7
Patrick Monnerat <patrick@monnerat.net>
2024-09-07 13:33:21 +0200

    Implement CURLOPT_SAFE_AUTH
    This option disables clear text password authentication mechanisms over
    a non-encrypted connection. In particular:
    - HTTP Basic authentication is disabled
    - PLAIN and LOGIN mechanisms are disabled for all SASL protocols
    - IMAP LOGIN is disabled
    - POP3 USER/PASS authentication is disabled
    - FTP authentication is disabled unless the user name is "anonymous"
    - LDAP simple bind is disabled if not anonymous.
    - SOCKS5 proxy basic authentication is disabled.
    
    TLS-encrypted connections (either implicit or explicit) are not affected
    as they won't transport clear text passwords.
    
    This option is triggered by the new curl tool options --safe-auth and
    --proxy-safe-auth.
    
    New tests 750-766 check this option in various contexts.

--- src/tool_operate.c
+++ src/tool_operate.c
@@ -1066,6 +1066,10 @@ static CURLcode config2setopts(struct GlobalConfig *global,
   if(config->authtype)
     my_setopt_bitmask(curl, CURLOPT_HTTPAUTH, (long)config->authtype);
 
+  /* new in libcurl 8.xx.x */
+  if(config->safeauth)
+    my_setopt(curl, CURLOPT_SAFE_AUTH, (long) config->safeauth);
+
   my_setopt_slist(curl, CURLOPT_HTTPHEADER, config->headers);
 
   if(proto_http || proto_rtsp) {
