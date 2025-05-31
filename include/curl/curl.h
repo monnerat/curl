869c0214c6775ef6949de8b3f6a9a6fc10faa34a
Patrick Monnerat <patrick@monnerat.net>
2025-05-31 13:08:27 +0200

    Implement CURLOPT_SAFE_AUTH
    This option disables clear text password authentication mechanisms over
    a non-encrypted connection. In particular:
    - HTTP Basic authentication is disabled
    - PLAIN and LOGIN mechanisms are disabled for all SASL protocols
    - IMAP LOGIN is disabled
    - POP3 USER/PASS authentication is disabled
    - FTP authentication is disabled unless the user name is "anonymous"
    - LDAP simple bind is disabled if not anonymous.
    - SOCKS5 proxy basic authentication is disabled.
    
    TLS-encrypted connections (either implicit or explicit) are not affected
    as they won't transport clear text passwords.
    
    This option is triggered by the new curl tool options --safe-auth and
    --proxy-safe-auth.
    
    New tests 760-776 check this option in various contexts.

--- include/curl/curl.h
+++ include/curl/curl.h
@@ -842,6 +842,10 @@ typedef enum {
 #define CURLAUTH_ANY          (~CURLAUTH_DIGEST_IE)
 #define CURLAUTH_ANYSAFE      (~(CURLAUTH_BASIC|CURLAUTH_DIGEST_IE))
 
+/* Bitmasks for CURLOPT_SAFE_AUTH. */
+#define CURLSAFE_AUTH         ((unsigned long)1 << 0) /* No unsafe mechs */
+#define CURLSAFE_PROXYAUTH    ((unsigned long)1 << 1) /* Same for proxy */
+
 #define CURLSSH_AUTH_ANY       ~0     /* all types supported by the server */
 #define CURLSSH_AUTH_NONE      0      /* none allowed, silly but complete */
 #define CURLSSH_AUTH_PUBLICKEY (1<<0) /* public/private key files */
@@ -2248,6 +2252,9 @@ typedef enum {
 
   /* set TLS supported signature algorithms */
   CURLOPT(CURLOPT_SSL_SIGNATURE_ALGORITHMS, CURLOPTTYPE_STRINGPOINT, 328),
+
+  /* Set secure authentication mechanisms flags. */
+  CURLOPT(CURLOPT_SAFE_AUTH, CURLOPTTYPE_LONG, 329),
 
   CURLOPT_LASTENTRY /* the last unused */
 } CURLoption;
